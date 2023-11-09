0e6be3f247cc1f2524665abffcca5202ec859186
FARAZIFTAN <147034569+FARAZIFTAN@users.noreply.github.com>
2023-11-09 22:35:28 +0700

    Tugas Asesment FARAZIFTAN

---
+++ 714230064_A.M. FARAZIFTAN/script.js
@@ -0,0 +1,25 @@
+function Paragrafchange() {
+    document.getElementById("desc").innerHTML =
+      "Mereka telah menjual beberapa buku di seluruh" +
+      "\r\n" +
+      "dunia sejak mereka menjual buku itu.";
+    document.getElementById(Image).src = "rumi.jpg";
+  }
+  
+  function Statement() {
+    var a, b, c;
+    a = 10;
+    b = 10;
+    c = a * b;
+    alert("Sejauh ini baru terjual sekitar" + c + "Buku");
+  }
+  
+  function AritmatikaSyntax() {
+    let a = 3;
+    let b = 4;
+  
+    let tambah = a + b;
+  
+    alert("Buku yang tersedia sekarang" + tambah + "Buku");
+  }
+  
